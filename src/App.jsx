4cbe1a9b6923869e98eb2d48870f3996dcf0895d
Daniel Gonzalez <danielgnzch@gmail.com>
2022-07-26 21:30:39 +0200

    Exercice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import animalData from './data.json';
 import Card from './components/Card';
 
 function App() {
+  const [animals, setAnimals] = useState(animalData);
+
+  const handleSortByLife = () => {
+    const ordered = [...animals].sort((a, b) => a.lifeExpectancy - b.lifeExpectancy)
+    setAnimals(ordered);
+  }
+
+  const handleFilterDogs = () => {
+    const filtered = animals.filter(elem => elem.type === "dog");
+    setAnimals(filtered);
+  }
 
   return (
     <div className="App">
       <h1>Adopt me plz 🐾</h1>
-      {/* Some code goes here */}
+      <button onClick={handleSortByLife} className="action-btn">Sort by life expectancy</button>
+      <button onClick={handleFilterDogs} className="action-btn">See only dogs</button>
+      {animals.map(elem => {
+        return <Card key={elem._id} animal={elem}/>
+      })}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-
 export default function Card(props) {
-  const { animal: { name, lifeExpectancy, description, needsMedicine, image } } = props;
-  
+  const {
+    animal: { name, lifeExpectancy, description, needsMedicine, image },
+  } = props;
+
   return (
     <div className="card">
       <h3>{name}</h3>
-      <img src={image} alt={name} width="100%"/>
+      {image ? (
+        <img src={image} alt={name} width="100%" />
+      ) : (
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Dog_silhouette.svg/2067px-Dog_silhouette.svg.png"
+          alt="Default Dog"
+          width="100%"
+        />
+      )}
       <p>{description}</p>
       <p>This animal will live {lifeExpectancy} years</p>
+      {needsMedicine && <p className="red-color">Needs medecine</p>}
     </div>
-  )
+  );
 }
